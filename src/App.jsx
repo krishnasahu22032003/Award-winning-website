f2a117e90f71deba57d158cd0b9bbc11ffb2705a
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-26 23:51:42 +0530

    Flavour title component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import SmoothScroll from "./components/SmoothScroll"
+import FlavorSection from "./sections/FlavorSection"
 import HeroSection from "./sections/HeroSection"
 import MessageSection from "./sections/MessageSection"
 
@@ -16,6 +17,7 @@ function App() {
      <div id="smooth-content">
 <HeroSection/>
 <MessageSection/>
+<FlavorSection/>
 
      </div>
    </div>
