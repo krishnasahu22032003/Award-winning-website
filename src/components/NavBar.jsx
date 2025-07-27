11c4245fe844f9a8efff7a8716f7ef9b37b8732e
Krishna Sahu <krishna.sahu.work@gmail.com>
2025-07-27 23:35:49 +0530

    Bug fixed again

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 const NavBar = () => {
   return (
     <nav className="fixed top-0 left-4 z-50 -mt-5  p-3">
-      <a href="home"><img src="/images/logo.png" alt="nav-logo" className="md:w-28 w-20" /></a>
+      <img src="/images/logo.png" alt="nav-logo" className="md:w-28 w-20" />
     </nav>
   );
 };
